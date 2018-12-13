626371988d941cda9fea3da03b88ec49daf79c6f
Petteri Uotila <petteri.uotila@helsinki.fi>
2018-12-13 13:35:30 +0200

    possibly a wrong arch-file

--- build-and-run-nemo3.6-ORCA1LIM3-sisu.csc.fi.sh
+++ build-and-run-nemo3.6-ORCA1LIM3-sisu.csc.fi.sh
@@ -28,32 +28,36 @@ find_root () {
 }
 
 cd NEMOGCM3.6/CONFIG
-cat > ../ARCH/arch-gnu-sisu.csc.fi.fcm <<EOF
-%CC                  cc
-%CFLAGS              -O0
-%CPP	             cpp
-%FC	             ftn
-%FCFLAGS             $(case $PE_ENV in (GNU) echo '-fdefault-real-8 -O3 -funroll-all-loops -fcray-pointer -ffree-line-length-none';; (CRAY) echo '-em -s real64 -s integer32  -O2 -hflex_mp=intolerant -e0 -ez';; esac)
-%FFLAGS              %FCFLAGS
-%LD                  ftn
-%LDFLAGS             -hbyteswapio
-%FPPFLAGS            -P -C -traditional-cpp
-%AR                  ar
-%ARFLAGS             -r
-%MK                  make
+cat > ../ARCH/arch-xc40-sisu.csc.fi.fcm <<EOF
+%HDF5_HOME           ${HDF5_DIR}
 
-%NCDF_HOME           $NETCDF_DIR
-%HDF5_HOME           $HDF5_DIR
-%XIOS_HOME           $(find_root xios)
-%OASIS_HOME          /not/defined
+%NCDF_HOME           ${NETCDF_DIR}
 %NCDF_INC            -I%NCDF_HOME/include -I%HDF5_HOME/include
-%NCDF_LIB            -L%NCDF_HOME/lib -lnetcdff -lnetcdf
+%NCDF_LIB            -L%HDF5_HOME/lib -L%NCDF_HOME/lib -lnetcdff -lnetcdf -lhdf5_hl -lhdf5 -lz
+
+%XIOS_HOME           /appl/climate/xios/2.0.990/CRAY/8.5
+#%XIOS_HOME           /appl/climate/xios/1.0.703/CRAY/8.5
 %XIOS_INC            -I%XIOS_HOME/inc
 %XIOS_LIB            -L%XIOS_HOME/lib -lxios
-%OASIS_INC           -I%OASIS_HOME/build/lib/mct -I%OASIS_HOME/build/lib/psmile.MPI1
-%OASIS_LIB           -L%OASIS_HOME/lib -lpsmile.MPI1 -lmct -lmpeu -lscrip
-%USER_INC            %XIOS_INC %OASIS_INC %NCDF_INC
-%USER_LIB            %XIOS_LIB %OASIS_LIB %NCDF_LIB
+
+%CPP                 cpp
+
+%FC                  ftn
+%FCFLAGS             -emf -s real64 -s integer32  -O2 -hflex_mp=intolerant -Rb 
+%FFLAGS              -emf -s real64 -s integer32  -O0 -hflex_mp=strict -Rb
+
+%FPPFLAGS            -P -E -traditional-cpp
+
+%LD                  ftn
+%LDFLAGS             -hbyteswapio 
+
+%AR                  ar
+%ARFLAGS             -r
+
+%MK                  gmake
+
+%USER_INC            %XIOS_INC
+%USER_LIB            %XIOS_LIB
 EOF
 
 # Get and set ORCA1L75LIM3 configuration without PISCES
@@ -66,7 +70,7 @@ rm ORCA1_LIM3/cpp_ORCA1_LIM3_PISCES.fcm
 echo "bld::tool::fppkeys key_trabbl key_lim3 key_vvl key_dynspg_ts key_diaeiv key_ldfslp key_traldf_c2d key_traldf_eiv key_dynldf_c3d  key_zdfddm key_zdftmx_new key_mpp_mpi key_zdftke key_iomput key_mpp_rep key_xios2 key_nosignedzero" > ORCA1_LIM3/cpp_ORCA1_LIM3.fcm
 
 # Compile executable
-./makenemo -m gnu-sisu.csc.fi -n ORCA1_LIM3
+./makenemo -m xc40-sisu.csc.fi -n ORCA1_LIM3
 
 # Create an experiment
 rm -f ORCA1_LIM3/EXP01
