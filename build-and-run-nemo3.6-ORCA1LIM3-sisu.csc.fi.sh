041e1111687f4d46cc9f81428db786edb70c249e
Petteri Uotila <petteri.uotila@helsinki.fi>
2018-12-13 11:58:14 +0200

    renamed to include a test run

--- build-and-run-nemo3.6-ORCA1LIM3-sisu.csc.fi.sh
+++ build-and-run-nemo3.6-ORCA1LIM3-sisu.csc.fi.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/bin/bash -l
+#SBATCH -J nemo
+#SBATCH -o nemo%J.out
+#SBATCH -e nemo%J.err
+#SBATCH -t 00:29:00
+#SBATCH -N 1
+#SBATCH -p test
 
 # Environment setup
 
@@ -7,7 +13,6 @@ module load svn craypkg-gen cray-hdf5-parallel cray-netcdf-hdf5parallel xios/2.0
 if [[ "$PE_ENV" = "GNU" ]]; then
     PE_LEVEL=5.1
 fi
-
 
 # Checkout sources
 
@@ -22,8 +27,8 @@ find_root () {
     echo ${BASH_REMATCH[1]}
 }
 
-cd trunk
-cat > arch/arch-gnu-sisu.csc.fi.fcm <<EOF
+cd NEMOGCM3.6/CONFIG
+cat > ../ARCH/arch-gnu-sisu.csc.fi.fcm <<EOF
 %CC                  cc
 %CFLAGS              -O0
 %CPP	             cpp
@@ -51,7 +56,38 @@ cat > arch/arch-gnu-sisu.csc.fi.fcm <<EOF
 %USER_LIB            %XIOS_LIB %OASIS_LIB %NCDF_LIB
 EOF
 
+# Get and set ORCA1L75LIM3 configuration without PISCES
+svn -r73 --username puotila co http://forge.ipsl.jussieu.fr/shaconemo/svn/trunk/ORCA1_LIM3_PISCES/
+echo "ORCA1_LIM3_PISCES OPA_SRC LIM_SRC_3 NST_SRC TOP_SRC" >> cfg.txt
+echo "ORCA1_LIM3 OPA_SRC LIM_SRC_3 NST_SRC" >> cfg.txt
 
-# Test
+cp -ar ORCA1_LIM3_PISCES ORCA1_LIM3
+rm ORCA1_LIM3/cpp_ORCA1_LIM3_PISCES.fcm
+echo "bld::tool::fppkeys key_trabbl key_lim3 key_vvl key_dynspg_ts key_diaeiv key_ldfslp key_traldf_c2d key_traldf_eiv key_dynldf_c3d  key_zdfddm key_zdftmx_new key_mpp_mpi key_zdftke key_iomput key_mpp_rep key_xios2 key_nosignedzero" > ORCA1_LIM3/cpp_ORCA1_LIM3.fcm
 
-./makenemo -m gnu-sisu.csc.fi -r 'GYRE_PISCES' -n 'MY_GYRE_PISCES'
+# Compile executable
+./makenemo -m gnu-sisu.csc.fi -n ORCA1_LIM3
+
+# Create an experiment
+mkdir ORCA1_LIM3/EXP01
+cd ORCA1_LIM3/EXP01
+tar -xf /wrk/puotila/DONOTREMOVE/SHACONEMO/INPUTS_ORCA1_LIM3_UH_V1.tar
+ln -s ${TMPDIR}/puotila/NEMOGCM3.6/CONFIG/ORCA1_LIM3/BLD/bin/nemo.exe opa
+ln -s ../../SHARED/namelist_ice_lim3_ref namelist_ice_ref
+ln -s ../../SHARED/namelist_ref namelist_ref
+
+cat > nemo_run.sh <<EOF
+#!/bin/bash -l
+#SBATCH -J nemo
+#SBATCH -o nemo%J.out
+#SBATCH -e nemo%J.err
+#SBATCH -t 00:29:00
+#SBATCH -N 1
+#SBATCH -p test
+
+module list
+
+aprun -n 24 ./opa
+EOF
+
+qsub nemo_run.sh
